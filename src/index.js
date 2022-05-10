7cf98d112a631e24491e1a401cda96f330905e94
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2022-05-10 16:35:13 +0300

    add single page github kin public index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
